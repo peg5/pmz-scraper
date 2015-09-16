99a2c478cd87c2fabd9d9c245227fee779246162
Patrick Graham <oddpoly@boun.cr>
2015-09-16 16:17:42 -0500

    Update pmz_new_release_artists.py
    Added license information in comments at the top.

--- pmz_new_release_artists.py
+++ pmz_new_release_artists.py
@@ -1,5 +1,21 @@
 #! python3
 
+#    pmz_new_release_artists.py
+#    Copyright (C) 2015  Patrick Graham
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import requests
 import bs4
 import csv
